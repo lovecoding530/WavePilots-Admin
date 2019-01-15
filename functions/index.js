03af6c77fdefc2febd920d12869bf13c9e91e022
Unknown <lovecoding530@gmail.com>
2019-01-15 11:14:06 -0800

    function send confirm email with template

--- functions/index.js
+++ functions/index.js
@@ -1,235 +1,83 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+const nodemailer = require('nodemailer');
+let ejs = require('ejs');
+admin.initializeApp();
 
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+// firebase functions:config:set gmail.email="myusername@gmail.com" gmail.password="secretpassword"
 
-exports.casinoChangeListener = functions.database.ref('casinos/{casinoId}')
-    .onWrite(event => {
-        var timeStamp = Math.floor(Date.now() / 1000);
-    
-        if (!event.data.exists()) {
-            // console.log('deleted casino');
-            return;
-        }
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
 
-        if (event.data.previous.exists()) {
-            var rawSnapshoot = event.data.child('raw');
-            if (rawSnapshoot.changed()) {
-                // console.log('update modified');
-                return event.data.ref.child('ref').update({
-                    modified: timeStamp
-                });
-            } else {
-                return;
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
+});
+
+const APP_NAME = 'WavePilots';
+
+const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for
+    // this URL must be whitelisted in the Firebase Console.
+    url: 'https://wavepilots-67f1c.firebaseapp.com/',
+    // This must be true for email link sign-in.
+    handleCodeInApp: true,
+};
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+    const email = user.email; // The email of the user.
+    const displayName = user.displayName; // The display name of the user.
+    return sendWelcomeEmail(email, displayName);
+});
+
+// Sends a welcome email to the given user.
+async function sendWelcomeEmail(email, displayName) {
+    var verificationLink = await admin.auth().generateEmailVerificationLink(email, actionCodeSettings);
+    verificationLink = removeParam('continueUrl', verificationLink);
+
+    let renderedHtml = await ejs.renderFile('./templates/welcome.ejs', {verificationLink});
+
+    const mailOptions = {
+        from: `${APP_NAME} <noreply@wavepilots.com>`,
+        replyTo: 'noreply@wavepilots.com',
+        to: email,
+        subject: `Welcome to ${APP_NAME}!`,
+        text: `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service. ${verificationLink}`,
+        html: renderedHtml
+    };
+    console.log(mailOptions);
+
+    await mailTransport.sendMail(mailOptions);
+    console.log('New welcome email sent to:', email);
+
+    return null;
+}
+
+/**
+ * remove parameter from url
+ */
+function removeParam(key, sourceURL) {
+    var resultUrl = sourceURL.split("?")[0];
+    var params = [];
+    var queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params = queryString.split("&");
+        for (var i = params.length - 1; i >= 0; i -= 1) {
+            let param = params[i].split("=")[0];
+            if (param === key) {
+                params.splice(i, 1);
             }
-        } else {
-            // console.log('created casino');
-            return event.data.ref.child('ref').update({
-                created: timeStamp,
-                modified: timeStamp,
-            });
         }
-    });
-
-exports.bonusChangeListener = functions.database.ref('bonuses/{casinoId}')
-    .onWrite(event => {
-        var timeStamp = Math.floor(Date.now() / 1000);
-    
-        if (!event.data.exists()) {
-            // console.log('deleted casino');
-            return;
-        }
-
-        if (event.data.previous.exists()) {
-            var rawSnapshoot = event.data.child('raw');
-            if (rawSnapshoot.changed()) {
-                // console.log('update modified');
-                return event.data.ref.child('ref').update({
-                    modified: timeStamp
-                });
-            } else {
-                return;
-            }
-        } else {
-            // console.log('created casino');
-            return event.data.ref.child('ref').update({
-                created: timeStamp,
-                modified: timeStamp,
-            });
-        }
-    });
-
-exports.slotChangeListener = functions.database.ref('slots/{slotId}')
-    .onWrite(event => {
-        var timeStamp = Math.floor(Date.now() / 1000);
-    
-        if (!event.data.exists()) {
-            // console.log('deleted casino');
-            return;
-        }
-
-        if (event.data.previous.exists()) {
-            var rawSnapshoot = event.data.child('raw');
-            if (rawSnapshoot.changed()) {
-                // console.log('update modified');
-                return event.data.ref.child('ref').update({
-                    modified: timeStamp
-                });
-            } else {
-                return;
-            }
-        } else {
-            // console.log('created casino');
-            return event.data.ref.child('ref').update({
-                created: timeStamp,
-                modified: timeStamp,
-            });
-        }
-    });
-
-exports.newsChangeListener = functions.database.ref('news/{newsId}')
-    .onWrite(event => {
-        var timeStamp = Math.floor(Date.now() / 1000);
-    
-        if (!event.data.exists()) {
-            // console.log('deleted casino');
-            return;
-        }
-
-        if (event.data.previous.exists()) {
-            var rawSnapshoot = event.data.child('raw');
-            if (rawSnapshoot.changed()) {
-                // console.log('update modified');
-                return event.data.ref.child('ref').update({
-                    modified: timeStamp
-                });
-            } else {
-                return;
-            }
-        } else {
-            // console.log('created casino');
-            return event.data.ref.child('ref').update({
-                created: timeStamp,
-                modified: timeStamp,
-            });
-        }
-    });
-
-exports.reviewChangeListener = functions.database.ref('reviews/{itemKey}/{userKey}')
-    .onWrite(event => {
-        
-        if (!event.data.exists()) {
-            console.log('Delete');
-            if (event.data.previous.exists()) {
-                var value = event.data.previous.val();
-                var parentName = value.Parent;
-                checkTopRate(parentName, event.params.itemKey);
-            }
-            return;
-        }
-
-        var databaseRef = event.data.ref;
-        if (event.data.previous.exists()) {
-            var value = event.data.previous.val();
-            var parentName = value.Parent;
-            var origineRate = value.Rate;
-            if (origineRate == null) {
-                origineRate = 0;
-            }
-            var newRate = event.data.val().Rate;
-            return new Promise((resolve, reject) => {
-                admin.database().ref(parentName + '/' + value.ItemKey)
-                    .once('value')
-                    .then(resItem => {
-                        var itemData = resItem.val();
-                        var refData = itemData.ref;
-                        var averageRate = 0;
-                        var rateUserCount = 1;
-                        if (refData != null) {
-                            averageRate = itemData.ref.averageRate;
-                            rateUserCount = itemData.ref.rateUserCount;
-                        }
-                        if (averageRate == null) {
-                            averageRate = 0;
-                        }
-                        if (rateUserCount == null) {
-                            rateUserCount = 1;
-                        }
-                        averageRate = averageRate + (newRate - origineRate) / rateUserCount;
-                        admin.database().ref(parentName + '/' + value.ItemKey)
-                            .child('ref')
-                            .update({
-                                averageRate: averageRate,
-                                rateUserCount: rateUserCount
-                            })
-                        // console.log('Review Item', resItem.val());
-                        checkTopRate(parentName, event.params.itemKey);
-                        resolve();
-                    })
-                    .catch(errorItem => {
-                        console.log('Review Item Error', errorItem);
-                        reject(errorItem);
-                    })
-            });
-        } else {
-            var value = event.data.val();
-            var parentName = value.Parent;
-            var newRate = value.Rate;
-            return new Promise((resolve, reject) => {
-                admin.database().ref(parentName + '/' + value.ItemKey)
-                    .once('value')
-                    .then(resItem => {
-                        var itemData = resItem.val();
-                        var refData = itemData.ref;
-                        var averageRate = 0;
-                        var rateUserCount = 1;
-                        if (refData != null) {
-                            averageRate = itemData.ref.averageRate;
-                            rateUserCount = itemData.ref.rateUserCount;
-                        }
-                        if (averageRate == null) {
-                            averageRate = 0;
-                        }
-                        if (rateUserCount == null) {
-                            rateUserCount = 1;
-                        } else {
-                            rateUserCount += 1;
-                        }
-                        averageRate = averageRate * (rateUserCount - 1) / rateUserCount + newRate / rateUserCount;
-                        admin.database().ref(parentName + '/' + value.ItemKey)
-                            .child('ref')
-                            .update({
-                                averageRate: averageRate,
-                                rateUserCount: rateUserCount
-                            })
-                        // console.log('Review Item', resItem.val());
-                        checkTopRate(parentName, event.params.itemKey);
-                        resolve();
-                    })
-                    .catch(errorItem => {
-                        console.log('Review Item Error', errorItem);
-                        reject(errorItem);
-                    })
-            });
-        }
-
-        function checkTopRate(parent, itemKey) {
-            console.log('checking top review....');
-            admin.database().ref('reviews/' + itemKey)
-                .orderByChild('Rate')
-                .limitToLast(1)
-                .once('child_added')
-                .then(reviewItem => {
-                    // console.log('Top Review : ', reviewItem.val());
-                    admin.database().ref(parent + '/' + itemKey + '/ref/topReview')
-                        .set(reviewItem.val())
-                        .then(topReviewRes => {})
-                        .catch(topReviewError => {
-                            console.log('Error write top review : ', topReviewError);
-                        })
-                })
-                .catch(reviewError => {
-                    console.log('Error Top Review : ', reviewError);
-                })
-        }
-    });
+        resultUrl = resultUrl + "?" + params.join("&");
+    }
+    return resultUrl;
+}
